4aabaf0c7f67510747eebf457bae8229c7ca7c47
GetSomeCats <76173678+Zeaphyou@users.noreply.github.com>
2023-04-17 00:07:47 +0800

    修改针对当前节点 修正不能正确结束问题

--- Net_Speedlite.js
+++ Net_Speedlite.js
@@ -72,9 +72,13 @@ let content = ''
   .catch(async e => {
     $.logErr(e)
     $.logErr($.toStr(e))
-    const result = { title, content, icon, 'icon-color': color, ...arg}
-  $.log($.toStr(result))
-    $.done()
+    const msg = ${$.lodash_get(e, 'message') || $.lodash_get(e, 'error') || e}
+    title = ❌
+    content = msg
+    await notify('网络速率', title, content)
+    const result = { title, content }
+    $.log($.toStr(result))
+    $.done(result)
   })
   .finally(async () => {
     const result = { title, content, icon, 'icon-color': color, ...arg}
